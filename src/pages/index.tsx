f620c25b64f423614707de3d507d020b7d0286d9
mateocuenca <mateorc099@gmail.com>
2023-02-26 20:33:28 -0300

    welcomeSection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Global.module.css";
+import WelcomeSection from "@/components/WelcomeSection";
 
 export default function Home() {
   return (
@@ -10,7 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="favicon.svg" />
       </Head>
-      <main className={styles.container}></main>
+      <main className={styles.container}>
+        <WelcomeSection />
+      </main>
     </>
   );
 }
